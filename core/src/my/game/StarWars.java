7e78628ee38f271fdc161a428b028772f5b8964e
Lopatin37 <Lopatin37@yandex.ru>
2020-02-27 01:04:57 +0300

    Сделал предыдущее дз. Есть пару вопросов. Когда я сделал дз за 2 задание, я реализовал это так, что иконка летела в точку touch не левым нижним углом, а серединой и так, что бы она не могла выходить за границы фона. Но, как я понял, в этом задании это не нужно было делать, вы сказали, что слишком усложнены вычисления. И второй момент, было указано, что, по возможности необходимо всю логику косающуюся логотипа перенести в отдельный класс Logo. Я не понял, что имеется ввиду под словом логотип и перенес туда все, что касается летящей иконки BadLogic. И еще, эту иконку я уменьшил, что бы наглядней было видно, как она перемещается в нужную точку.

--- core/src/my/game/StarWars.java
+++ core/src/my/game/StarWars.java
@@ -1,12 +1,10 @@
 package my.game;
 
-
 import com.badlogic.gdx.Game;
-
 import my.game.screen.MenuScreen;
 
+public class StarWars extends Game {
 
-public class StarWars extends Game{
     @Override
     public void create() {
         setScreen(new MenuScreen());
